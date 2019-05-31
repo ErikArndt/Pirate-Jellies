758b4bf04a04851998b1f90e4ba84ab8e115b7ad
CalebArndt <36738371+CalebArndt@users.noreply.github.com>
2019-05-31 18:11:41 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
 def linesCross (line1S, line1E, line2S, line2E, ray):
+    #Gives you the point of intersection for two line segments, or one line segment and a ray.
+    #The output is given as a set of coordinates (x, y)
+    #If there is no intersection, it will output (-1, -1)
+    #line1S is the starting coordinates for the first line, given as (x, y). line1E is the ending coordinates, and so on.
+    #ray is a boolean. If true, then line 1 is a ray, and will continue on infinitely past its ending coordinates
     if line1S[0] > line1E[0]:
         line1S, line1E = line1E, line1S
     if line2S[0] > line2E[0]:
@@ -92,6 +97,9 @@ def linesCross (line1S, line1E, line2S, line2E, ray):
 
 
 def rectIntersect(rect1, rect2):
+    #Tells you whether two rectangles overlap at any point.
+    #rect1 is the coordinates of the top left corner of the rectangle, as well as its width and height, (x, y, width, height).
+    #The output is a boolean, True if the rectangles overlap.
     x1, y1, width1, height1 = rect1
     x2, y2, width2, height2 = rect2
     if ((x1 > x2 and x1 < x2+width2) or (x1+width1 > x2 and x1+width1 < x2+width2)) and ((y1 > y2 and y1 < y2+height2) or (y1+height1 > y2 and y1+height1 < y2+height2)):
